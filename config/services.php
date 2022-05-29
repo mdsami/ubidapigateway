1f71f1a2733d265363f633a0bafc6fc158f5c2e7
mdsami <mdsami.work@gmail.com>
2022-05-29 10:18:33 +0600

    ubid micro added

--- config/services.php
+++ config/services.php
@@ -30,4 +30,40 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'cms' => [
+        'base_uri' => env('CMS_SERVICE_BASE_URL'),
+        'secret' => env('CMS_SERVICE_SECRET'),
+    ],
+
+    'admin' => [
+        'base_uri' => env('ADMIN_SERVICE_BASE_URL'),
+        'secret' => env('ADMIN_SERVICE_SECRET'),
+    ],
+    'citizen' => [
+        'base_uri' => env('CITIZEN_SERVICE_BASE_URL'),
+        'secret' => env('CITIZEN_SERVICE_SECRET'),
+    ],
+
+    'workflow' => [
+        'base_uri' => env('WORKFLOW_SERVICE_BASE_URL'),
+        'secret' => env('WORKFLOW_SERVICE_SECRET'),
+    ],
+
+    'application' => [
+        'base_uri' => env('APPLICATON_SERVICE_BASE_URL'),
+        'secret' => env('APPLICATON_SERVICE_SECRET'),
+    ],
+
+    'si' => [
+        'base_uri' => env('SI_SERVICE_BASE_URL'),
+        'secret' => env('SI_SERVICE_SECRET'),
+    ],
+
+    'generator' => [
+        'base_uri' => env('GENERATOR_SERVICE_BASE_URL'),
+        'secret' => env('GENERATOR_SERVICE_SECRET'),
+    ],
+
+
+
 ];
